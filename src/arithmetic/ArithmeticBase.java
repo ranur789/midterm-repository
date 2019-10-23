cf5b01e8020fd70bce6f0281378c34372c02fb4d
ranur789 <ranur@sheridancollege.ca>
2019-10-22 20:31:39 -0400

    midterm

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -15,8 +15,9 @@ import java.util.Scanner;
  */
 public class ArithmeticBase 
 {
- public double x,y;
+private  double x,y;
     double calculate(double x, double y) 
+           
         {
         Scanner sc =new Scanner(System.in);
         System.out.println("enter String");
@@ -34,6 +35,34 @@ public class ArithmeticBase
             default:
                 throw new AssertionError("Unknown operations " + this);
         }
+    }
+
+    /**
+     * @return the x
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(double y) {
+        this.y = y;
     }
    
 }
